f1f3237b644ed9f994670bfb25819b43324651e9
MirineTest <miriamnhq@gmail.com>
2020-10-02 22:42:35 -0400

    unit

---
+++ src/test/java/configuration/Configuration.java
@@ -0,0 +1,14 @@
+package configuration;
+
+/**
+ * @autor : eynar.pari
+ * @date : 26/09/2020.
+ **/
+public class Configuration {
+
+    public static final String HOST="http://todo.ly/";
+    //  user:pwd ---> Basic base64
+    public static final String BASIC_AUTHENTICATION="Basic TWlyaUB0ZXN0LmNvbTpQYXNzd29yZDEh";//mio tengo q poner
+    public static final String BASIC_AUTHENTICATION_HEADER="Authorization";
+
+}
